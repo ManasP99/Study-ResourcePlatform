0f39d8c6340154e5d64f6f2f8cd69b92852c7556
ManasP99 <167458832+ManasP99@users.noreply.github.com>
2026-05-02 02:49:36 +0530

    Refactor server.js to implement resource and task APIs

--- server/server.js
+++ server/server.js
@@ -134,10 +134,7 @@ app.use(express.urlencoded({ extended: true }));
 
 /*MONGODB CONNECT*/
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => {
     console.log("MongoDB Error:", err);
